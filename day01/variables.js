e3eb1ec2aba7f27233f52c409140ab4064070ca3
linxlone666 <monmhoo99@gmail.com>
2026-07-30 23:21:16 +0700

    learned: varieable

---
+++ day01/variables.js
@@ -0,0 +1,19 @@
+// varieable declaration
+var name = "John Doe";
+var age = 30;
+var isStudent = true;
+
+// let & const declaration
+let city = "New York";
+const country = "USA";
+
+// var can be redeclared and updated but let and const cannot be redeclared in the same scope. However, let can be updated while const cannot be updated after its initial assignment.
+
+// varieable types
+var number = 42; // number, can be integer or float
+var string = "Hello, World!"; // string
+var boolean = true; // boolean
+var array = [1, 2, 3, 4, 5]; // array
+var object = { key: "value" }; // object is a collection of key-value pairs
+var nullValue = null; // null is kinda not there but it is a value that represents the intentional absence of any object value.
+var undefinedValue; // undefined is very not there, it is a value that represents the absence of a value. It is the default value of uninitialized variables.
