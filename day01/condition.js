8c9fe1d36eb7f8f0270765a222bc2028f5aabce8
linxlone666 <monmhoo99@gmail.com>
2026-07-31 18:59:59 +0700

    Learned: conditions

---
+++ day01/condition.js
@@ -0,0 +1,33 @@
+// this is how conditionals work in JavaScript
+let age = 20;
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not an adult.");
+}
+
+// if else if else statement
+let score = 85;
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
+
+// switch statement
+let day = 3;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("Invalid day");
+}
