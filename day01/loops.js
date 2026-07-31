e9fe6cee601880fc37cd28bd76dfee8871d33583
linxlone666 <monmhoo99@gmail.com>
2026-07-31 19:02:46 +0700

    Learned: for loops, while loop, do while loop,

---
+++ day01/loops.js
@@ -0,0 +1,19 @@
+// this is loops in JavaScript
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration: " + i);
+}
+
+// while loop
+let j = 0;
+while (j < 5) {
+  console.log("Iteration: " + j);
+  j++;
+}
+
+// do while loop
+let k = 0;
+do {
+  console.log("Iteration: " + k);
+  k++;
+} while (k < 5);
